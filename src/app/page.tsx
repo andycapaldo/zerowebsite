cccea143530223be0b79393cceca6e4df3013c36
andycapaldo <andycapaldo2@gmail.com>
2025-09-04 16:59:28 -0600

    removed scrollbar from bgvid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import coolvid from '/videos/3051492-hd_1920_1080_25fps.mp4?thumbnailTime=0'
 
 export default function Home() {
   return (
-    <div className="h-screen p-0 m-0">
+    <div className="h-[100dvh] w-screen overflow-hidden">
       <div className='max-h-[80] text-white'>
         <BackgroundVideo controls={true} src={coolvid}>
           <Image alt="zero logo" src={zerologomain} />

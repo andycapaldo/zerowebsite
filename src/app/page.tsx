edad9b243ea8b5be82eb8f170f62257314061533
Andrew Capaldo <andycapaldo2@gmail.com>
2025-09-09 14:03:16 -0600

    Added motion with animations on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ import BackgroundVideo from 'next-video/background-video';
 import coolvid from '/videos/3051492-hd_1920_1080_25fps.mp4'
 import LinkButton from './components/LinkButton';
 import { faMedium, faXTwitter, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { motion, stagger } from "motion/react"
 
 
 export default function Home() {
-
+  
   return (
     <div className="relative min-h-[100dvh] h-screen w-screen overflow-hidden bg-black/95 selection:bg-slate-300/30 selection:text-slate-900">
         <BackgroundVideo 
@@ -21,26 +22,41 @@ export default function Home() {
               <div className='flex-none'>
                 <div className='max-w-6xl mx-auto text-center space-y-8'>
 
-                  <div className='flex justify-center'>
+                  <motion.div 
+                    className='flex justify-center'
+                    initial={{ y: -50, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 3.0, delay: 1.0, ease: "easeOut" }}
+                  >
                     <Image 
                     alt="zero logo" 
                     src={zerologomain} 
                     className='w-auto h-auto max-w-full'
                     priority
                     />
-                  </div>
-
-                  <h1 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white'>
-                    Frictionless and Stable Microtransactions
-                  </h1>
+                  </motion.div>
+                  <motion.div
+                    initial={{ y: -50, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 3.0, delay: 3.0, ease: "easeOut" }}
+                  >
+                    <h1 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white'>
+                      Frictionless and Stable Microtransactions
+                    </h1>
+                  </motion.div>
                 </div>
               </div>
               <div className='flex-[2] text-white'>
-                <div className='flex flex-col mt-12 lg:flex-row items-center justify-center w-full'>
-                  <LinkButton icon={faXTwitter} label="X" />
-                  <LinkButton icon={faMedium} label="Medium" />
-                  <LinkButton icon={faDiscord} label="Discord" />
-                </div>
+                <motion.div 
+                  className='flex flex-col mt-12 lg:flex-row items-center justify-center w-full'
+                  initial={{ y: -50, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 3.0, delay: 5.0, ease: "easeOut"}}
+                  >
+                    <LinkButton icon={faXTwitter} label="X" />
+                    <LinkButton icon={faMedium} label="Medium" />
+                    <LinkButton icon={faDiscord} label="Discord" />
+                </motion.div>
               </div>
             </div>
           </div>

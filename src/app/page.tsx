a904ff3e3ff7a89c15b5cc27946ea3e219fbdc93
Andrew Capaldo <andycapaldo2@gmail.com>
2025-09-09 18:21:29 -0600

    Updated animations, added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Image from 'next/image'
 import zerologomain from '../../public/zerologomain.png';
+import zerologo from '../../public/zerologo.png';
 import BackgroundVideo from 'next-video/background-video';
 import coolvid from '/videos/3051492-hd_1920_1080_25fps.mp4'
 import LinkButton from './components/LinkButton';
@@ -11,7 +12,7 @@ import { motion, stagger } from "motion/react"
 export default function Home() {
   
   return (
-    <div className="relative min-h-[100dvh] h-screen w-screen overflow-hidden bg-black/95 selection:bg-slate-300/30 selection:text-slate-900">
+    <div className="relative min-h-[100dvh] h-screen w-screen overflow-hidden bg-black/95 selection:bg-slate-300/30 selection:text-slate-900 text-white" >
         <BackgroundVideo 
           controls={false} 
           src={coolvid}
@@ -26,7 +27,7 @@ export default function Home() {
                     className='flex justify-center'
                     initial={{ y: -50, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
-                    transition={{ duration: 3.0, delay: 1.0, ease: "easeOut" }}
+                    transition={{ duration: 1.0, delay: 1.0, ease: "easeOut" }}
                   >
                     <Image 
                     alt="zero logo" 
@@ -38,24 +39,43 @@ export default function Home() {
                   <motion.div
                     initial={{ y: -50, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
-                    transition={{ duration: 3.0, delay: 3.0, ease: "easeOut" }}
+                    transition={{ duration: 1.0, delay: 2.0, ease: "easeOut" }}
                   >
-                    <h1 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white'>
+                    <h1 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl'>
                       Frictionless and Stable Microtransactions
                     </h1>
                   </motion.div>
                 </div>
               </div>
-              <div className='flex-[2] text-white'>
+              <div className='flex-[2]'>
                 <motion.div 
                   className='flex flex-col mt-12 lg:flex-row items-center justify-center w-full'
                   initial={{ y: -50, opacity: 0 }}
                   animate={{ y: 0, opacity: 1 }}
-                  transition={{ duration: 3.0, delay: 5.0, ease: "easeOut"}}
+                  transition={{ duration: 1.0, delay: 5.0, ease: "easeOut"}}
                   >
                     <LinkButton icon={faXTwitter} label="X" />
                     <LinkButton icon={faMedium} label="Medium" />
                     <LinkButton icon={faDiscord} label="Discord" />
+                </motion.div>
+              </div>
+              <div className='flex-[3] h-screen'>
+                <motion.div 
+                  className='flex flex-col justify-center items-center mt-36'
+                  initial={{ y: -50, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 1.0, delay: 6.0, ease: "easeOut" }}
+                >
+                  <div className="w-60 md:w-80 lg:w-120 h-0.5 bg-gradient-to-r from-transparent via-white to-transparent"></div>
+                  <div>
+                    <Image 
+                      alt="zero logo" 
+                      src={zerologo} 
+                      className='mt-16 w-16 h-16 mx-auto'
+                      priority
+                    />
+                    © 2025 Zero All rights reserved.
+                  </div>
                 </motion.div>
               </div>
             </div>
